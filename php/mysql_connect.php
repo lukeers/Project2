f89ddc0103e9b600695256d1cb11da28c6ca3110
DevilGale <nkatz1@umbc.edu>
2016-11-09 17:05:46 -0500

    Putting Files into Folders
    Putting all files into folders of the previous team.

--- php/mysql_connect.php
+++ php/mysql_connect.php
@@ -8,14 +8,14 @@
 /*DEFINE('DB_HOST', 'localhost');
 DEFINE('DB_USER', 'root');
 DEFINE('DB_PASSWORD', '');
-DEFINE('DB_NAME', 'XXXXX');
+DEFINE('DB_NAME', 'david52');
 
 $conn = @mysql_connect(DB_HOST, DB_USER, DB_PASSWORD) or die("Could not con\
 nect to MySQL");
 $rs = @mysql_select_db(DB_NAME, $conn) or die("Could not connect select $db database");*/
 
 
-$conn = @mysql_connect("studentdb-maria.gl.umbc.edu", "XXXXXX", "XXXXXXX") or die("Could not connect to MySQL");
-$rs = @mysql_select_db("XXXXXXX", $conn) or die("Could not connect select $db database");
+$conn = @mysql_connect("studentdb-maria.gl.umbc.edu", "david52", "david52") or die("Could not connect to MySQL");
+$rs = @mysql_select_db("david52", $conn) or die("Could not connect select $db database");
 
 ?>
