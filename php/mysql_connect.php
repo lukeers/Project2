a928a899f74d14b0e97cfe798a2a49e833c87b08
chensel1 <kyameron343@gmail.com>
2016-11-30 12:16:08 -0500

    Cameron Design Changes

---
+++ php/mysql_connect.php
@@ -0,0 +1,21 @@
+<!-- mysql_connect.php -->
+<!-- This file holds the information about connecting to the database that holds the necessary information -->
+
+<?php
+
+//Database connection under user "userweb"
+
+/*DEFINE('DB_HOST', 'localhost');
+DEFINE('DB_USER', 'root');
+DEFINE('DB_PASSWORD', '');
+DEFINE('DB_NAME', 'david52');
+
+$conn = @mysql_connect(DB_HOST, DB_USER, DB_PASSWORD) or die("Could not con\
+nect to MySQL");
+$rs = @mysql_select_db(DB_NAME, $conn) or die("Could not connect select $db database");*/
+
+
+$conn = @mysql_connect("studentdb-maria.gl.umbc.edu", "david52", "david52") or die("Could not connect to MySQL");
+$rs = @mysql_select_db("david52", $conn) or die("Could not connect select $db database");
+
+?>
