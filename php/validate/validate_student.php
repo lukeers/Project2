316613931ff0fba2e42384b6e38c66ba7c895f4d
David Reichard <david52@umbc.edu>
2016-12-04 19:46:10 -0500

    Added validation to students, removed username

--- php/validate/validate_student.php
+++ php/validate/validate_student.php
@@ -3,83 +3,41 @@
 
 <?php
 require_once('../mysql_connect.php');
-
-// Finds all the usernames from the database
-$sql = "SELECT Username FROM students";
+// Finds all the student IDs from the database
+$sql = "SELECT studentID FROM students";
 $rs = mysql_query($sql, $conn);
 
 //By default no errors
 $errors = False;
 $error_message = "";
 
-//Loop through usernames, check for match
-while($username = mysql_fetch_array($rs)) 
+//Loop through student IDs, check for match
+while($IDs = mysql_fetch_array($rs)) 
 {
-  if ($_POST['username'] == $username['Username']) 
+  if ($_POST['studentID'] == $IDs['studentID']) 
   {
     //Match found - BAD - there is an error
     $errors = True;
-    $error_message = "Username already taken<br>";
+    $error_message = "User ID already taken<br>";
     break;
   }
-}
-
-//Username left blank check
-if ($_POST['username'] == "") 
-{
-  $errors = True;
-  $error_message .= "Username field can't be blank.<br>";
-}
-
-//Major left blank check
-if ($_POST['major'] == "") 
-{
-  $errors = True;
-  $error_message .= "Major field can't be left blank.<br>";
-}
-
-// First name left blank check
-if ($_POST['firstName'] == "")
-{
-  $errors = True;
-  $error_message .= "First Name field can't be left blank.<br>";
-}
-
-// Last name left blank check
-if ($_POST['lastName'] == "")
-{
-  $errors = True;
-  $error_message .= "Last Name field can't be left blank.<br>";
-}
-
-// StudentID left blank check
-if ($_POST['studentID'] == "") 
-{
-  $errors = True;
-  $error_message .= "Student ID field can't be left blank.<br>";
-}
-
-// email left blank check
-if ($_POST['email'] == "")
-{
-  $errors = True;
-  $error_message .= "Email field can't be left blank.<br>";
 }
 
 if ($errors != True) 
 {
   //No errors - GOOD - Insert into database
-  $sql = "INSERT INTO students (username, major, email, firstName, lastName, studentID) VALUES (\"" . $_POST['username'] . "\", \"" . $_POST['major'] . "\", \"" . $_POST['email'] . "\", \"" . $_POST['firstName'] . "\", \"" . $_POST['lastName'] . "\", \"" . $_POST['studentID'] . "\")";
+  $sql = "INSERT INTO students (major, email, firstName, lastName, studentID) VALUES (\"" . $_POST['major'] . "\", \"" . $_POST['email'] . "\", \"" . $_POST['firstName'] . "\", \"" . $_POST['lastName'] . "\", \"" . $_POST['studentID'] . "\")";
   $rs = mysql_query($sql, $conn);
   session_start();
-  $_SESSION['username'] = $_POST['username'];
+  $_SESSION['studentID'] = $_POST['studentID'];
 
   // Go to the student_view.php file
   header('Location:../view/student_view.php');
 }
 else
 {
-  // Go to the register_student_error.html file
-  require('../../html/error_forms/register_student_error.html');
+  // Go to the login_student.html file
+  echo($error_message);
+  require('../../html/forms/login_student.html');
 }
 ?>
