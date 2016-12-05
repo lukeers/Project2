fd72d316c0a4d1d508af2de0dbfb97e2666b1e58
David Reichard <david52@umbc.edu>
2016-12-04 19:44:43 -0500

    Revert "Added form validation to student"
    This reverts commit 1512269a009351ef2042b06bcd5d394ae93b07d6.

--- php/validate/validate_student.php
+++ php/validate/validate_student.php
@@ -3,8 +3,9 @@
 
 <?php
 require_once('../mysql_connect.php');
+
 // Finds all the usernames from the database
-$sql = "SELECT studentID FROM students";
+$sql = "SELECT Username FROM students";
 $rs = mysql_query($sql, $conn);
 
 //By default no errors
@@ -12,24 +13,66 @@ $errors = False;
 $error_message = "";
 
 //Loop through usernames, check for match
-while($IDs = mysql_fetch_array($rs)) 
+while($username = mysql_fetch_array($rs)) 
 {
-  if ($_POST['studentID'] == $IDs['studentID']) 
+  if ($_POST['username'] == $username['Username']) 
   {
     //Match found - BAD - there is an error
     $errors = True;
-    $error_message = "User ID already taken<br>";
+    $error_message = "Username already taken<br>";
     break;
   }
+}
+
+//Username left blank check
+if ($_POST['username'] == "") 
+{
+  $errors = True;
+  $error_message .= "Username field can't be blank.<br>";
+}
+
+//Major left blank check
+if ($_POST['major'] == "") 
+{
+  $errors = True;
+  $error_message .= "Major field can't be left blank.<br>";
+}
+
+// First name left blank check
+if ($_POST['firstName'] == "")
+{
+  $errors = True;
+  $error_message .= "First Name field can't be left blank.<br>";
+}
+
+// Last name left blank check
+if ($_POST['lastName'] == "")
+{
+  $errors = True;
+  $error_message .= "Last Name field can't be left blank.<br>";
+}
+
+// StudentID left blank check
+if ($_POST['studentID'] == "") 
+{
+  $errors = True;
+  $error_message .= "Student ID field can't be left blank.<br>";
+}
+
+// email left blank check
+if ($_POST['email'] == "")
+{
+  $errors = True;
+  $error_message .= "Email field can't be left blank.<br>";
 }
 
 if ($errors != True) 
 {
   //No errors - GOOD - Insert into database
-  $sql = "INSERT INTO students (major, email, firstName, lastName, studentID) VALUES (\"" . $_POST['major'] . "\", \"" . $_POST['email'] . "\", \"" . $_POST['firstName'] . "\", \"" . $_POST['lastName'] . "\", \"" . $_POST['studentID'] . "\")";
+  $sql = "INSERT INTO students (username, major, email, firstName, lastName, studentID) VALUES (\"" . $_POST['username'] . "\", \"" . $_POST['major'] . "\", \"" . $_POST['email'] . "\", \"" . $_POST['firstName'] . "\", \"" . $_POST['lastName'] . "\", \"" . $_POST['studentID'] . "\")";
   $rs = mysql_query($sql, $conn);
   session_start();
-  $_SESSION['studentID'] = $_POST['studentID'];
+  $_SESSION['username'] = $_POST['username'];
 
   // Go to the student_view.php file
   header('Location:../view/student_view.php');
