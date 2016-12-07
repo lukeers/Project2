fdfed23e24c028b109363b8d3b56fd635c1daa8c
DevilGale <nkatz1@umbc.edu>
2016-12-07 17:20:23 -0500

    Fix: Student Update
    Fixes so meeting is pushed to student

--- php/table_handler.php
+++ php/table_handler.php
@@ -2,51 +2,50 @@
 <!-- This file contains the information the will update the tables after a students signs up for an appointment -->
 
 <?php
-include('mysql_connect.php');
-$apptID = $_POST['ID'];
-echo $apptID;
-session_start();
+  include('mysql_connect.php');
+  $apptID = $_POST['ID'];
+  echo $apptID;
+  session_start();
 
-//query for isFull calc
-$sql = "SELECT isGroup, NumStudents FROM appointments WHERE id=$apptID";
-echo $sql;
-$rs = mysql_query($sql, $conn);
-$appt = mysql_fetch_array($rs);
+  //query for isFull calc
+  $sql = "SELECT isGroup, NumStudents FROM appointments WHERE id=$apptID";
+  echo $sql;
+  $rs = mysql_query($sql, $conn);
+  $appt = mysql_fetch_array($rs);
 
-// This checks if the appointment is a group appointment 
-if ($appt['isGroup'] == 1) {
-  
-  // This is if is Group is true
-  if ($appt['NumStudents'] == 9) {
+  // This checks if the appointment is a group appointment
+  if ($appt['isGroup'] == 1) {
 
-    // This is if the amount of students is one before full, so now that one more student is added 
-    // isFull will be changed to true
-    $sql = "UPDATE appointments SET isFull=1, NumStudents=(NumStudents+1) WHERE id=$apptID";
-    mysql_query($sql, $conn);
+    // This is if is Group is true
+    if ($appt['NumStudents'] == 9) {
+
+      // This is if the amount of students is one before full, so now that one more student is added
+      // isFull will be changed to true
+      $sql = "UPDATE appointments SET isFull=1, NumStudents=(NumStudents+1) WHERE id=$apptID";
+      mysql_query($sql, $conn);
+    } else {
+
+      // group but not full yet
+      echo "UPdating appoitnments table group<br/>";
+      $sql = "UPDATE appointments SET NumStudents=(NumStudents+1) WHERE id=$apptID";
+      mysql_query($sql, $conn);
+
+    }
   } else {
 
-    // group but not full yet
-    echo "UPdating appoitnments table group<br/>";
-    $sql = "UPDATE appointments SET NumStudents=(NumStudents+1) WHERE id=$apptID";
+    // Not a group
+    echo "Updating appointments table not Group<br/>";
+    $sql = "UPDATE appointments SET NumStudents=1, isFull=1 WHERE id=$apptID";
     mysql_query($sql, $conn);
 
   }
-} else {
-  
-  // Not a group
-  echo "Updating appointments table not Group<br/>";
-  $sql = "UPDATE appointments SET NumStudents=1, isFull=1 WHERE id=$apptID";
+
+  echo "Update students table <br/>";
+    //update student
+  $sql = "UPDATE students SET Appt ='$apptID' WHERE studentID='" . $_SESSION['studentID'] . "'";
+  echo "<br>Students Table:" . $sql;
   mysql_query($sql, $conn);
 
-}
-
-echo "updateing students table <br/>";
-  //update student
-  $sql = "UPDATE students SET Appt = $apptID WHeRE Username=\"" . $_SESSION['username'] . "\"";
-echo $sql;
-
-  mysql_query($sql, $conn);
-  
-  header('Location:view/student_view.php');
+  //header('Location:view/student_view.php');
 
 ?>
