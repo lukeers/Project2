c2e02066c71ef19f9e85d43a30cce595ed5936f6
David Reichard <david52@umbc.edu>
2016-12-13 15:13:35 -0500

    Fixed register_advisor.php

--- html/forms/register_advisor.php
+++ html/forms/register_advisor.php
@@ -12,9 +12,9 @@
 -->
     <?php
 	include("../../php/advisor_nav_bar.php");
-	if($_session['error_message']){
-	   echo($_session['error_message']);
-	   unset($_session['error_message']);
+	if(isset($_SESSION['error_message'])){
+	   echo($_SESSION['error_message']);
+	   unset($_SESSION['error_message']);
 	}
     ?>
 
@@ -23,11 +23,10 @@
     <form method=post action='../../php/validate/validate_advisor.php'>
       <!-- Gets information from the potential advisor, username, first name, last name -->
       <p>Username: <input type=text name="username"/></p>
-	  <p>First Name: <input type=text name="fName"/></p>
-	  <p>Last Name: <input type=text name="lName"/></p>
-	  <p>Password: <input type=text name="password"/></p>
-	  <p>Office: <input type=text name="office"/></p>
+      <p>First Name: <input type=text name="fName"/></p>
+      <p>Last Name: <input type=text name="lName"/></p>
+      <p>Password: <input type=text name="password"/></p>
+      <p>Office: <input type=text name="office"/></p>
       <p><input type=submit value="Submit"/></p>
     </form>
-	<!-- Hyperlink to the login advisor page -->
-	<p>Already signed up? Click <a href = "login_advisor.html">here</a> to log in.</p>
+
