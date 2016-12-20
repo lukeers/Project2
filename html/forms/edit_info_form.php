d04b21c89291fcaaeb475bbe3bb8fed91cece28c
DevilGale <nkatz1@umbc.edu>
2016-12-19 21:29:31 -0500

    Update advisor profile

--- html/forms/edit_info_form.php
+++ html/forms/edit_info_form.php
@@ -2,19 +2,19 @@
 <!-- This file will get a username from the user then compare it to the database, if there is a match then the user will be logged in -->
 
 <?php
-	include('../php/advisor_nav_bar.php');
+	include('../../php/advisor_nav_bar.php');
 ?>
 <html>
 <h1>Change Advisor Info</h1>
-<h2>(All of these fields are optional, you don't have to fill them in)</h2>
-<form name="advisoreditform" method=post action='/validate/validate_change_advisor.php' onsubmit="return validateEditForm();">
+<p><b>(All of these fields are optional, you don't have to fill them in)</b></p>
+<form name="advisoreditform" method=post action='../../php/validate/validate_update_advisor.php' onsubmit="return validateEditForm();">
 	<!-- Gets information from the potential advisor, username, first name, last name -->
 	<p>New Username: <input type=text name="username" /></p>
 	<p>New Name: <input type=text name="fullName" /></p>
 	<p>New Password: <input type=password name="password" /></p>
 	<p>Confirm Password: <input type=password name="confirm" /></p>
 	<p id="passErr"></p>
-	
+
 	<p><input type=submit id="submit" class="button" value="Submit"/></p>
 </form>
 
--- php/validate/validate_update_advisor.php
+++ php/validate/validate_update_advisor.php
@@ -3,17 +3,22 @@
 require_once("../mysql_connect.php");
 session_start();
 
+echo $_SESSION['username'];
+echo "username" . empty($_POST['username']) . $_POST['username'] . "<br>";
+echo "password" . empty($_POST['password']) . $_POST['password'] ."<br>";
+echo "fullname" . empty($_POST['fullName']) . $_POST['fullName'] ."<br>";
+
 //If username is to be updated
-if(isset($_POST['username']))
+if(!empty($_POST['username']))
 {
   //Update advisor database to show username change
   $sql = "UPDATE advisors SET Username='". $_POST['username'] ."' WHERE Username='". $_SESSION['username'] ."'";
-	$rs = mysql_query($sql, $conn);
+	mysql_query($sql, $conn);
   echo "<br>" . $sql . "<br>";
 
   //Update Appointments to show username change
   $sql = "UPDATE appointments SET AdvisorUsername='". $_POST['username'] ."' WHERE AdvisorUsername='". $_SESSION['username'] ."'";
-	$rs = mysql_query($sql, $conn);
+	mysql_query($sql, $conn);
   echo "<br>" . $sql . "<br>";
 
   //Update session username to show change
@@ -21,30 +26,31 @@ if(isset($_POST['username']))
 }
 
 //If password field is to be updated
-if(isset($_POST['password']))
+if(!empty($_POST['password']))
 {
-  if($_POST['password'] == $_POST['passwordCheck'])
+  if($_POST['password'] == $_POST['confirm'])
   {
     $sql = "UPDATE advisorpasswords SET password='". sha1($_POST['password']). "' WHERE username='". $_SESSION['username'] ."'";
-    $rs = mysql_query($sql, $conn);
+    mysql_query($sql, $conn);
     echo "<br>" . $sql . "<br>";
   }
 }
 
 //If Advisor name is to be update
-if(isset($_POST['fullname']))
+if(!empty($_POST['fullName']))
 {
   //Update advisor database to show username change
-  $sql = "UPDATE advisors SET fullName='". $_POST['fullname'] ."' WHERE Username='". $_SESSION['username'] ."'"
-  $rs = mysql_query($sql, $conn);
-  echo "<br>" . $sql . "<br>";
+  $sql = "UPDATE advisors SET fullName='". $_POST['fullName'] ."' WHERE Username='". $_SESSION['username'] ."'";
+  mysql_query($sql, $conn);
+  echo ("<br>" . $sql . "<br>");
 
   //Update Appointments to show username change
-  $sql = "UPDATE appointments SET Advisor='". $_POST['fullname'] ."' WHERE AdvisorUsername='". $_SESSION['username'] ."'"
-  $rs = mysql_query($sql, $conn);
+  $sql = "UPDATE appointments SET Advisor='". $_POST['fullName'] ."' WHERE AdvisorUsername='". $_SESSION['username'] ."'";
+  mysql_query($sql, $conn);
   echo "<br>" . $sql . "<br>";
 }
 
-//header("../view/advisor_view.php");
+header("../view/advisor_view.php");
 
  ?>
+<a href="../view/advisor_view.php">Back</a>
